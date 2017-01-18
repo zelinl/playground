f83b59eef9296f57f671b78e6fc8b2a48618d30b
ubuntu <lvzelin@varkj.com>
2017-01-18 17:00:00 +0800

    01182017

--- python/alien/alien_invasion.py
+++ python/alien/alien_invasion.py
@@ -1,8 +1,8 @@
-import sys
 import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     pygame.init()
@@ -13,12 +13,7 @@ def run_game():
 
     while True:
         # 监听键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-            screen.fill(ai_settings.bg_color)
-            ship.blitme()
-            pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
