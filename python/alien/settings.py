1370984e24a4a5f15086de7847e5782edd3fa28b
Zelin Lyu <zelinl@gmail.com>
2017-07-17 23:59:17 +0800

    delete python tutorial project

--- python/alien/settings.py
+++
@@ -1,9 +0,0 @@
-class Settings():
-    """设置类"""
-
-    def __init__(self):
-        """初始化游戏设置"""
-        # 屏幕
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (222,222,222)
